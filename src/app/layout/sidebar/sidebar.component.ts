b207f3570cd997c274885bfd3c9146bbfa9391ec
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2021-11-08 20:11:57 -0600

    :construction: Agregando dropdown menus

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-  constructor() {}
+  linkMenus = document.querySelectorAll('.menu__link');
 
-  ngOnInit(): void {}
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    // this.renderer.listen(this.el.nativeElement.addEventListener('.menu__link'), (event) => {
+    //   console.log('Click');
+    // });
+  }
+
+  activeMenu(event: Event) {
+    console.log(event);
+    // this.linkMenus.forEach((menu: Element) => {
+    //   menu.classList.remove('menu__link--active');
+    // });
+  }
+
+  dropdown(event: any) {
+    this.linkMenus.forEach((menu) => {
+      menu.classList.remove('menu__dropdown-link--active');
+    });
+    event.target.classList.add('menu__dropdown-link--active');
+  }
 }
