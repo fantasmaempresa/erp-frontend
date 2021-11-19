c973a73d18e904504c04fab6e557b93d7a46eeb7
raitoforce <mahgt_34@hotmail.com>
2021-11-19 17:46:12 -0600

    :construction: añado formulario de cliente a modulo

--- src/app/features/clients/clients-routing.module.ts
+++ src/app/features/clients/clients-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientFormComponent } from './client-form/client-form.component';
 import { ListClientsComponent } from './list-clients/list-clients.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ListClientsComponent,
-    children: [{ path: 'add', component: ListClientsComponent, data: { breadcrumb: 'Agregar' } }],
+    component: ClientFormComponent,
+    children: [
+      { path: 'add', component: ListClientsComponent, data: { breadcrumb: 'Agregar Cliente' } },
+    ],
   },
 ];
 
--- src/app/features/clients/clients.module.ts
+++ src/app/features/clients/clients.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ListClientsComponent } from './list-clients/list-clients.component';
+import { ClientFormComponent } from './client-form/client-form.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [ListClientsComponent],
-  imports: [CommonModule, ClientsRoutingModule],
+  declarations: [ListClientsComponent, ClientFormComponent],
+  imports: [SharedModule, ClientsRoutingModule],
 })
 export class ClientsModule {}
