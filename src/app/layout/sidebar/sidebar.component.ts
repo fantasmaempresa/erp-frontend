cf2bf1a2ff774cef960628d0714a7e26b3fe0d28
Raitoforce34 <mahgt_34@hotmail.com>
2021-11-18 20:34:07 -0600

    :bug: arreglo del sidebar

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,78 +1,57 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit, AfterViewInit {
-  source = '';
-
-  linkMenus!: NodeListOf<Element>;
-
-  dropdowns!: NodeListOf<Element>;
-
-  dropdownIsVisible = false;
-
+export class SidebarComponent {
   menus = [
     {
       menuName: 'Menu',
       submenus: [
         { label: 'Home', route: '/dashboard', icon: 'home' },
         { label: 'Usuarios', route: '/clients', icon: 'person' },
-        { label: 'Roles', route: '/clients', icon: 'verified_user' },
+        { label: 'Roles', route: '/roles', icon: 'verified_user' },
         { label: 'Clientes', route: '/clients', icon: 'people' },
-        { label: 'Personal', route: '/clients', icon: 'people' },
-        { label: 'Áreas', route: '/clients', icon: 'people' },
+        { label: 'Personal', route: '/staff', icon: 'groups' },
+        { label: 'Áreas', route: '/areas', icon: 'group_work' },
         {
           label: 'Proyectos',
           icon: 'rule_folder',
+          isOpen: false,
           dropdowns: [
-            { label: 'Bandeja de Entrada', route: '/clients', icon: 'mail' },
-            { label: 'Reenviar', route: 'profile', icon: 'reply' },
+            { label: 'Pendientes', route: '/pending_projects', icon: 'pause' },
+            { label: 'En Curso', route: '/ongoing_projects', icon: 'play_arrow' },
           ],
         },
         {
-          label: 'Tramites',
-          icon: 'rule_folder',
+          label: 'Trámites',
+          icon: 'description',
+          isOpen: false,
           dropdowns: [
-            { label: 'Bandeja de Entrada', route: '/clients', icon: 'mail' },
-            { label: 'Reenviar', route: 'profile', icon: 'reply' },
+            { label: 'Pendientes', route: '/pending_procedures', icon: 'pause' },
+            { label: 'En Curso', route: '/ongoing_procedure', icon: 'play_arrow' },
           ],
         },
       ],
     },
   ];
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  ngOnInit(): void {
-    // this.renderer.listen(this.el.nativeElement.addEventListener('.menu__link'), (event) => {
-    //   console.log('Click');
-    // });
-  }
-
-  ngAfterViewInit(): void {
-    this.linkMenus = this.el.nativeElement.querySelectorAll('.menu__link');
-  }
-
-  activeMenu(event: Event) {
-    this.linkMenus.forEach((menu) => {
-      console.log(menu);
-      this.renderer.removeClass(menu, 'menu__link--active');
+  dropdown(submenu: any) {
+    this.menus.forEach((menu) => {
+      menu.submenus.forEach((subMenu) => {
+        subMenu.isOpen = false;
+      });
     });
-    this.renderer.addClass(event.target, 'menu__link--active');
+    submenu.isOpen = true;
   }
 
-  dropdown(source: string) {
-    this.source = source;
-    this.linkMenus.forEach((menu) => {
-      this.renderer.removeClass(menu, 'menu__dropdown-link--active');
-    });
-    this.dropdownIsVisible = !this.dropdownIsVisible;
+  closeSubmenu(submenu: any) {
+    submenu.isOpen = false;
   }
 
-  log() {
-    console.log('CLICK');
+  stopPropagation(event: Event) {
+    event.stopPropagation();
   }
 }
