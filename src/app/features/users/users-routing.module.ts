14ca9a9b72498036f50053f1ffb2e075c9870145
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2021-12-06 14:35:23 -0600

    :construction: Separo la configuración en un submenu para englobar usuarios y roles por ahora

--- src/app/features/users/users-routing.module.ts
+++ src/app/features/users/users-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildrenRouteLayoutComponent } from '../../layout/children-route-layout/children-route-layout.component';
+import { UserListComponent } from './user-list/user-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: UserFormComponent,
+    component: ChildrenRouteLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full',
+      },
+      {
+        path: 'list',
+        component: UserListComponent,
+        data: { breadcrumb: 'Lista de usuarios' },
+      },
+      {
+        path: 'new',
+        component: UserFormComponent,
+        data: { breadcrumb: 'Nuevo usuario' },
+      },
+    ],
   },
 ];
 
--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { UserFormComponent } from './user-form/user-form.component';
 import { SharedModule } from '../../shared/shared.module';
 import { UsersRoutingModule } from './users-routing.module';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
-  declarations: [UserFormComponent],
+  declarations: [UserFormComponent, UserListComponent],
   imports: [SharedModule, UsersRoutingModule],
 })
 export class UsersModule {}
