31f0e6e78035761f47e371fd17158324afd09374
Raitoforce34 <mahgt_34@hotmail.com>
2022-03-11 13:53:29 -0600

    🚚 add new module and card-view component

---
+++ src/app/shared/components/dinamyc-views/dynamic-views.module.ts
@@ -0,0 +1,24 @@
+import { InjectionToken, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MemoizedSelector } from '@ngrx/store';
+import { TableViewComponent } from './table-view/table-view.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { CardViewComponent } from './card-view/card-view.component';
+
+@NgModule({
+  declarations: [TableViewComponent, CardViewComponent],
+  imports: [CommonModule, MatTableModule, MatCheckboxModule, MatPaginatorModule],
+  exports: [TableViewComponent],
+})
+export class DynamicViewsModule {}
+
+export const SELECTOR = new InjectionToken<MemoizedSelector<any, any>>('selector');
+export const LOAD_ACTION = new InjectionToken<any>('load_action');
+export const LOAD_NEXT_ACTION = new InjectionToken<(props: { size: number; page: number }) => any>(
+  'load_next_action',
+);
+export const LABELS = new InjectionToken<string[]>('labels');
+export const FIELDS = new InjectionToken<string[]>('fields');
+export const ACTION_KEY = new InjectionToken<string>('action_key');
