2803ca8c65e6f33e108aacf326df1d6536dd40b7
Raitoforce34 <mahgt_34@hotmail.com>
2022-03-11 13:11:25 -0600

    🎨 reformat code

--- src/app/shared/components/DynamicViews/generic-table/generic-table.component.spec.ts
+++ src/app/shared/components/DynamicViews/generic-table/generic-table.component.spec.ts
@@ -3,14 +3,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GenericTableComponent } from './generic-table.component';
 
 describe('GenericTableComponent', () => {
-  let component: GenericTableComponent;
-  let fixture: ComponentFixture<GenericTableComponent>;
+  let component: GenericTableComponent<any>;
+  let fixture: ComponentFixture<GenericTableComponent<any>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GenericTableComponent ]
-    })
-    .compileComponents();
+      declarations: [GenericTableComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
