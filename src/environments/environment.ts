3b80368cb40d699ba9db9e931e686af292ee1580
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2022-03-29 18:58:13 -0600

    :closed_lock_with_key: Modifico secrets por cambios en el back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   base_url: 'http://174.138.49.219:8080/api',
-  client_id: '95e484d7-d8a9-45d4-a0f0-c272e414db95',
-  client_secret: 'GzK8U1bI0FTCbS0lDS3jZ3gl2tbU9ehpBfQR7V4f',
+  client_id: '95f0774e-6775-4f8a-867b-8cb07e80ab2e',
+  client_secret: 'YwmDtrT0bKtiNAM8RJvq7nZtU7pRJxTT80eCxYqH',
   grant_type: 'password',
   mix_pusher_app_key: '1234567',
   mix_pusher_app_cluster: 'mt1',
