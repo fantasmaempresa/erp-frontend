9444d7108c506b0bad8c9f6282dbaf873a1fc281
Raitoforce34 <mahgt_34@hotmail.com>
2022-04-04 15:09:00 -0500

    📱 add responsive

--- src/app/shared/components/dinamyc-views/change-view/change-view.component.ts
+++ src/app/shared/components/dinamyc-views/change-view/change-view.component.ts
@@ -1,12 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { ActionsCard } from '../card-view/card-view.component';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-change-view',
   templateUrl: './change-view.component.html',
   styleUrls: ['./change-view.component.scss'],
 })
-export class ChangeViewComponent {
+export class ChangeViewComponent implements OnInit, OnDestroy {
+  hiddenSelector = false;
+
+  private unsubscribeSubject = new Subject<boolean>();
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
   isSelected: boolean = false;
 
   @Input()
@@ -16,5 +24,22 @@ export class ChangeViewComponent {
 
   setSelectedItem(item: any) {
     this.selectedItem.emit(item);
+  }
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe(['(max-width: 1024px)'])
+      .pipe(takeUntil(this.unsubscribeSubject))
+      .subscribe({
+        next: (state: BreakpointState) => {
+          this.hiddenSelector = state.matches;
+          this.isSelected = state.matches;
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeSubject.next(true);
+    this.unsubscribeSubject.complete();
   }
 }
