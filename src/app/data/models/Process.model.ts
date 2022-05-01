a1b6b33c38804a921a33a186c18cd0e8339fd4b3
Raitoforce34 <mahgt_34@hotmail.com>
2022-05-01 13:48:44 -0500

    🚧 json config ready

--- src/app/data/models/Process.model.ts
+++ src/app/data/models/Process.model.ts
@@ -23,4 +23,27 @@ export class Process extends EntityModel {
     this.description = description;
     this.config = config;
   }
+
+  static mapConfig(order: any, phases_process: any[]) {
+    const { order_phases: orderPhase } = order;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const order_phases = phases_process.reduce((acc, current, index) => {
+      const { id } = current;
+      const previousId = orderPhase[index].previous ?? null;
+      const next = index + 1 <= phases_process.length ? phases_process[index + 1] : null;
+
+      return [
+        ...acc,
+        {
+          phase: { id },
+          next: { phase: next ? { id: next.id } : null },
+          previous: {
+            phase: previousId ? { id: previousId } : null,
+          },
+          end_process: orderPhase[index].end_process,
+        },
+      ];
+    }, []);
+    return { order_phases, phases_process: phases_process.map(({ id }) => ({ id })) };
+  }
 }
--- src/app/features/process/page/build-process/build-process.component.ts
+++ src/app/features/process/page/build-process/build-process.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injector } from '@angular/core';
+import { Component, forwardRef, Injector, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import {
   CLAZZ,
@@ -14,14 +14,29 @@ import {
 } from '../../../../state/process-phase/processPhase.actions';
 import { selectProcessPhase } from '../../../../state/process-phase/processPhase.selector';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import {
+  ControlValueAccessor,
+  FormArray,
+  FormControl,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { debounceTime, Subject, take, takeUntil } from 'rxjs';
+import { Process } from '../../../../data/models/Process.model';
 
 @Component({
   selector: 'app-build-process',
   templateUrl: './build-process.component.html',
   styleUrls: ['./build-process.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => BuildProcessComponent),
+      multi: true,
+    },
+  ],
 })
-export class BuildProcessComponent {
+export class BuildProcessComponent implements ControlValueAccessor, OnDestroy {
   processPhases!: ProcessPhase[];
 
   orderFormArray = new FormArray([]);
@@ -30,13 +45,28 @@ export class BuildProcessComponent {
     order_phases: this.orderFormArray,
   });
 
+  private onDestroy$ = new Subject<number>();
+
+  constructor(private dialog: MatDialog) {
+    this.form.valueChanges
+      .pipe(debounceTime(100), takeUntil(this.onDestroy$))
+      .subscribe((value) => this.notifyValueChange(value));
+  }
+
+  private notifyValueChange(value: any) {
+    this.onChange(Process.mapConfig(value, this.processPhases));
+    this.onTouch();
+  }
+
+  onChange = (_: any) => {};
+
+  onTouch = () => {};
+
   onlyPrevious = (array: any[], [index]: [number]) => {
     return array.reduce((acc, currentItem, i) => {
       return i < index ? [...acc, currentItem] : acc;
     }, []);
   };
-
-  constructor(private dialog: MatDialog) {}
 
   openDialog() {
     const inj = Injector.create({
@@ -56,25 +86,43 @@ export class BuildProcessComponent {
       },
     });
 
-    dialogRef.afterClosed().subscribe((processPhases: any[]) => {
-      this.processPhases = processPhases;
-      this.orderFormArray.clear();
-      this.processPhases.forEach(() => {
-        this.orderFormArray.push(
-          new FormGroup({
-            end_process: new FormControl(),
-            next: new FormControl(),
-          }),
-        );
+    dialogRef
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((processPhases: any[]) => {
+        this.processPhases = processPhases;
+        this.orderFormArray.clear();
+        this.processPhases.forEach(() => {
+          this.orderFormArray.push(
+            new FormGroup({
+              end_process: new FormControl(false),
+              previous: new FormControl(),
+            }),
+          );
+        });
       });
-    });
   }
 
   drop(event: CdkDragDrop<any, any>) {
     moveItemInArray(this.processPhases, event.previousIndex, event.currentIndex);
     const currentGroup = this.orderFormArray.at(event.previousIndex);
     this.orderFormArray.removeAt(event.previousIndex);
-    currentGroup.get('next')?.setValue(null);
+    currentGroup.get('previous')?.setValue(null);
     this.orderFormArray.insert(event.currentIndex, currentGroup);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  writeValue(obj: any): void {}
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next(1);
+    this.onDestroy$.complete();
   }
 }
