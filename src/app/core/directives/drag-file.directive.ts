2a7d2a5b6c8087f682fd267fec08ab6ebda29673
raitoforce <mahgt_34@hotmail.com>
2022-07-26 10:26:29 -0500

    🎨 clean comments

--- src/app/core/directives/drag-file.directive.ts
+++ src/app/core/directives/drag-file.directive.ts
@@ -7,15 +7,10 @@ export class DragFileDirective {
   // eslint-disable-next-line @angular-eslint/no-output-on-prefix
   @Output() onFileDropped = new EventEmitter<any>();
 
-  // @HostBinding('style.background-color') private background = '#f5fcff';
-  // @HostBinding('style.opacity') private opacity = '1';
-
   // Dragover listener
   @HostListener('dragover', ['$event']) onDragOver(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
-    // this.background = '#9ecbec';
-    // this.opacity = '0.8';
   }
 
   // Dragleave listener
@@ -23,8 +18,6 @@ export class DragFileDirective {
   public onDragLeave(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
-    // this.background = '#f5fcff';
-    // this.opacity = '1';
   }
 
   // Drop listener
@@ -32,8 +25,6 @@ export class DragFileDirective {
   public ondrop(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
-    // this.background = '#f5fcff';
-    // this.opacity = '1';
     const { files } = evt.dataTransfer ?? { files: [] };
     if (files.length > 0) {
       this.onFileDropped.emit(files);
