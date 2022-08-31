6157562f1397392a311b8825b8a707dec8e10a05
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2022-08-31 16:31:25 -0500

    :closed_lock_with_key: Actualizo secrets por cambios en el servidor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://34.94.51.135/api',
-  client_id: '9707e6c5-f86b-4dcb-b6c3-1061e2137d36',
-  client_secret: 'RO6MJwCFgvBzklM4btxOPlzF86SLiXEwawW7lrPm',
+  base_url: 'http://35.236.46.69/api',
+  client_id: '972635d9-b530-4007-8040-9801dbccf813',
+  client_secret: 'za2ZwQx1DEi4XXuLgsMJt9pUCMRyFlFzH5qIct0w',
   grant_type: 'password',
   mix_pusher_app_key: '1234567',
   mix_pusher_app_cluster: 'mt1',
