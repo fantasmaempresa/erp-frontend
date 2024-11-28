b81c699c0ae706789c0ef00815534d0f8640dcfc
Alejandro Rodríguez Serrano <36872513+AlexMatix@users.noreply.github.com>
2024-11-28 17:25:36 -0600

    projects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  socket_url: 'notaria4puebla.com',
-  socket_port: 6003,
   base_url: 'https://back.notaria4puebla.com.mx/api',
   client_id: '9c55588d-b42c-4daa-ba0f-60a77d203895',
   client_secret: 'bRVNwTLRVccFPdmdVka18PGVoIaxQoCghVZyzxoV',
   grant_type: 'password',
   mix_pusher_app_key: '2221asdf',
   mix_pusher_app_cluster: 'mt1',
+  socket_url: 'notaria4puebla.com',
+  socket_port: 6003,
 };
