bd020ae8688b311b77e1b199c6bebee69a08142c
Alejandro Rodríguez Serrano <36872513+AlexMatix@users.noreply.github.com>
2024-11-29 11:52:51 -0600

    :bug: sockets

--- src/app/core/classes/laravel-web-socket.ts
+++ src/app/core/classes/laravel-web-socket.ts
@@ -15,7 +15,7 @@ export abstract class LaravelWebSocket {
       broadcaster: 'pusher',
       key: environment.mix_pusher_app_key,
       cluster: environment.mix_pusher_app_cluster,
-      forceTLS: false,
+      forceTLS: true,
       wsHost: environment.socket_url,
       wsPort: environment.socket_port,
       wssPort: environment.socket_port,
